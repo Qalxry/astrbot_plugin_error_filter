ca374cc953bb3ec3e79bd0e7278cfaa2bd2e7671
Qalxry <70196852+Qalxry@users.noreply.github.com>
2025-02-11 01:48:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from astrbot.api import logger
 from astrbot.api.provider import LLMResponse
 from openai.types.chat.chat_completion import ChatCompletion
 
-@register("error_filter", "LuffyLSX", "屏蔽机器人的错误信息回复。", "1.0.0")
+@register("error_filter", "Qalxry (fork from LuffyLSX)", "指定机器人的错误信息。", "1.0.1")
 class ErrorFilter(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
         self.config = config
         self.IsError_filter = self.config.get('IsError_filter', True)
+        self.Error_reply = self.config.get('Error_reply', '')
     
     @filter.on_decorating_result()
     async def on_decorating_result(self, event: AstrMessageEvent):
@@ -20,4 +21,6 @@ class ErrorFilter(Star):
         if self.IsError_filter:
             if '请求失败' in message_str:
                 logger.info(message_str)
-                event.stop_event() # 停止回复
+                if self.Error_reply == '':
+                    event.stop_event() # 停止回复
+                result.chain = [Plain(self.Error_reply)]
