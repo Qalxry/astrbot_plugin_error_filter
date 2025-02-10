ae3d444a6125f545bd853a17b8c8793fce8c420d
Qalxry <qalxry@qq.com>
2025-02-11 02:08:19 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from astrbot.api.platform import AstrBotMessage
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from astrbot.api.provider import LLMResponse
+from astrbot.core.message.components import Plain
 from openai.types.chat.chat_completion import ChatCompletion
 
 @register("error_replacer", "Qalxry", "指定机器人的错误信息。", "1.0")
@@ -23,4 +24,5 @@ class ErrorFilter(Star):
                 logger.info(message_str)
                 if self.Error_reply == '':
                     event.stop_event() # 停止回复
-                result.chain = [Plain(self.Error_reply)]
+                result.chain.clear()
+                result.message(self.Error_reply)
